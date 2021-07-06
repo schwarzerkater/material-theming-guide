db9886186bd2e8ad106e6c8b31313784edaf91c8
Peter Maier <peter.maier@iteratec.de>
2021-07-06 13:12:36 +0200

    Add some more examples (and hacky layout)
    - Toolbar
    - Input with error
    - Color grid

--- material-theming-guide/src/app/app.module.ts
+++ material-theming-guide/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { StatusIndicatorComponent } from './status-indicator/status-indicator.component'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input'
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ColorGridComponent } from './color-grid/color-grid.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatusIndicatorComponent
+    StatusIndicatorComponent,
+    ColorGridComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ material-theming-guide/src/app/color-grid/color-grid.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-grid',
+  templateUrl: './color-grid.component.html',
+  styleUrls: ['./color-grid.component.scss']
+})
+export class ColorGridComponent {}
